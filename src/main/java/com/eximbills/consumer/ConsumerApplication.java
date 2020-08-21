6d759290b5e80e4cf0aec17c806d53b831616cb1
Dayu Bian <dayub@chinasystems.com>
2020-08-21 13:51:37 +0800

    project init

---
+++ src/main/java/com/eximbills/consumer/ConsumerApplication.java
@@ -0,0 +1,44 @@
+package com.eximbills.consumer;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ConsumerApplication {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(ConsumerApplication.class);
+
+	private final SimpleDateFormat SDF = new SimpleDateFormat("HH:mm:ss");
+
+	private static final String HOSTNAME = System.getenv().getOrDefault("HOSTNAME", "unknow").replace("\n", " - ")
+			.trim();
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerApplication.class, args);
+	}
+
+	@PostMapping("/")
+	@ResponseBody
+	public String consume(String cloudEventJson) {
+		JSONObject response = new JSONObject(cloudEventJson).put("host", HOSTNAME).put("time", SDF.format(new Date()));
+		LOGGER.info("Event Message Received \n {}", response.toString());
+		return response.toString();
+	}
+
+	@GetMapping("/healthz")
+    public String health() {
+        return "OK";
+    }
+}
