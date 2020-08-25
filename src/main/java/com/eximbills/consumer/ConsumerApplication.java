849bf08f84b4aa709f4011d6791af097ff4025cd
dayub@chinasystems.com <dayu@DELL-Precision3630.localdomain>
2020-08-25 16:15:25 +0800

    Message to CloudEvents

--- src/main/java/com/eximbills/consumer/ConsumerApplication.java
+++ src/main/java/com/eximbills/consumer/ConsumerApplication.java
@@ -1,18 +1,25 @@
 package com.eximbills.consumer;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.Map;
 
-import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import io.cloudevents.CloudEvent;
+import io.cloudevents.v03.AttributesImpl;
+import io.cloudevents.v03.http.Unmarshallers;
 
 @SpringBootApplication
 @RestController
@@ -20,21 +27,34 @@ public class ConsumerApplication {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(ConsumerApplication.class);
 
-	private final SimpleDateFormat SDF = new SimpleDateFormat("HH:mm:ss");
-
-	private static final String HOSTNAME = System.getenv().getOrDefault("HOSTNAME", "unknow").replace("\n", " - ")
-			.trim();
-
 	public static void main(String[] args) {
 		SpringApplication.run(ConsumerApplication.class, args);
 	}
 
 	@PostMapping("/")
-	public String consume(@RequestBody String cloudEventJson) {
-		JSONObject response = new JSONObject(cloudEventJson).put("receiver", HOSTNAME).put("receive-time",
-				SDF.format(new Date()));
-		LOGGER.info("Event Message Received \n {}", response.toString());
-		return response.toString();
+	public ResponseEntity<Void> consume(@RequestHeader Map<String, Object> headers, @RequestBody String body) {
+		LOGGER.info("Received request header: " + headers);
+		LOGGER.info("Received request body: " + body);
+		try {
+			Object contentTypeVal = headers.get("Content-Type");
+			if (contentTypeVal == null
+					|| MediaType.APPLICATION_JSON_VALUE.equalsIgnoreCase(contentTypeVal.toString())) {
+				@SuppressWarnings("rawtypes")
+				CloudEvent<AttributesImpl, Map> cloudEvent = Unmarshallers.binary(Map.class).withHeaders(() -> headers)
+						.withPayload(() -> body).unmarshal();
+			    AttributesImpl attributesImpl = cloudEvent.getAttributes();
+				LOGGER.info("Received CloudEvent: " + cloudEvent);
+				LOGGER.info("ID: " + attributesImpl.getId());
+				LOGGER.info("Spec Version: " + attributesImpl.getSpecversion());
+				LOGGER.info("Subject: " + attributesImpl.getSubject());
+				LOGGER.info("Time" + attributesImpl.getTime());
+			}
+			return ResponseEntity.accepted().build();
+		} catch (Exception e) {
+			String errMsg = "ERROR: Exception processing received event";
+			LOGGER.error(errMsg, e);
+			throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, errMsg, e);
+		}
 	}
 
 	@GetMapping("/healthz")
